2d9c8b7fdc4795be3db4a669d42b86c265283ed4
Snap7 <chanturidzegiorgi31@gmail.com>
2023-09-30 14:07:17 +0400

    User registration with DRF to create serializers and views for registration

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import CustomUserSerializer
 
-# Create your views here.
+class RegisterUserView(generics.CreateAPIView):
+    serializer_class = CustomUserSerializer
