29d4a1ee0ac9b3a30fe2ba32e66e00ebd5398fcb
Snap7 <chanturidzegiorgi31@gmail.com>
2023-09-30 13:56:04 +0400

    Update Django settings.py

--- book_giveaway/settings.py
+++ book_giveaway/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'book_giveaway.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'book_giveaway',
+        'USER': 'admin',
+        'PASSWORD': 'secretpassword',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
